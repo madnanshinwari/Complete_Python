0b72e5ff923ad5eebf3a003f69def20616b1831f
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2025-01-29 14:48:37 +0500

    operators

---
+++ Basics/operator.py
@@ -0,0 +1,21 @@
+"""
+Numeric Expression or Operator :
+
+Operator : Operation 
++        :  Addition
+-        :  Subtraction
+*        :  Multiplication
+/        :  Division
+**       :  Power
+%        :  Remainder   
+
+"""
+x = 20
+y = 5
+
+print("Addition:- ",x+y) # 25
+print("Subtraction: ",x-y) # 15 
+print("Multiplication: ",x*y) # 100
+print("Divison: ",x/y) # 4.0
+print(" Power: ",x**y) # 3200000
+print("Remainder: ",x%y) # 0 

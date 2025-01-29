d81ad713d279a95e3120c08780380628a0a3f82c
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2025-01-29 14:30:06 +0500

    basics of python

---
+++ Basics/Variable.py
@@ -0,0 +1,19 @@
+""" Variable : A variable is a named place in the memory where a programmer can store data and later reterive the data using the variable "name".
+--> Programmer get to choose the names of the variables.
+--> You can change the contents of a variable in a later statement. 
+
+Rules for Variable Name:
+--> Must start with a letter or underscore ( _ ).
+--> Must consist of letters, numbers, and underscores.
+--> Case Sensitive.
+
+"""
+
+
+
+
+student_marks = 90
+# print(student_marks)
+
+student_marks = 78  # change the variable
+print(student_marks)

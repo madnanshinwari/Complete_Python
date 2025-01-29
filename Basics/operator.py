3f309754b2c9b371337eadacf3afabb8e2d1e667
Muhammad Adnan Khan <98257145+Mr-Adnankhan@users.noreply.github.com>
2025-01-29 14:50:21 +0500

    operators

--- Basics/operator.py
+++ Basics/operator.py
@@ -13,9 +13,9 @@ Operator : Operation
 x = 20
 y = 5
 
-print("Addition:- ",x+y) # 25
-print("Subtraction: ",x-y) # 15 
-print("Multiplication: ",x*y) # 100
-print("Divison: ",x/y) # 4.0
-print(" Power: ",x**y) # 3200000
-print("Remainder: ",x%y) # 0 
+print("Addition:- ",x+y) #   25
+print("Subtraction: ",x-y) #    15 
+print("Multiplication: ",x*y) #   100
+print("Divison: ",x/y) #  4.0
+print(" Power: ",x**y) #  3200000
+print("Remainder: ",x%y) #  0 
